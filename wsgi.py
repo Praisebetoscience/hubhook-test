2a8f0bc47f3875872a99c9d5beef0080c91eb614
praisebetoscience <pbts.reddit@gmail.com>
2017-01-26 09:20:55 +0000

    Initial commit.

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from bottle import request, run, route, Response
+
+@route('/')
+def index():
+    return Response('Build: 1')
+
+if __name__ == "__main__":
+    run(host='0.0.0.0',
+        port='3000',
+        server='gunicorn',
+        debug=True,
+        reloader=True,
+        workers=1)
